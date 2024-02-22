be9792c4547287b980c0453e57c996d6a54cce7a
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2024-02-22 12:17:52 -0800

    Add error message

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const btn = document.getElementById("btn");
 
+btn.addEventListener("click", () => {
+    const id = document.getElementById("id");
+    const advice = document.getElementById("advice");
+    fetch("https://api.adviceslip.com/advice")
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.json();
+        })
+        .then((data) => {
+            id.innerText = `ADVICE #${data.slip.id}`;
+            advice.innerText = data.slip.advice;
+        })
+        .catch((error) => {
+            id.innerText = `ERROR`;
+            advice.innerText = error.message;
+        });
+});
